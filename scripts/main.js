dc6c79c9ea14dc1145b0a38f847642e923853fdf
Sweaty-AKA-Dr <109566073+Sweaty-AKA-Dr@users.noreply.github.com>
2023-03-01 14:37:24 -0500

    done

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+const system = server.registerSystem(0, 0);
+
+system.listenForEvent("minecraft:player_message", (eventData) => {
+    const message = eventData.data.message;
+    const player = eventData.data.sender;
+    
+    if (message === "-spawn") {
+        system.executeCommand(`tag "${player.name}" add spawn`, () => {});
+        eventData.canceled = true;
+    } else if (message === "-sell") {
+        system.executeCommand(`tag "${player.name}" add sell`, () => {});
+        eventData.canceled = true;
+    } else if (message === "-shop") {
+        system.executeCommand(`tag "${player.name}" add shop`, () => {});
+        eventData.canceled = true;
+    } else if (message === "-plots") {
+        system.executeCommand(`tag "${player.name}" add plots`, () => {});
+        eventData.canceled = true;
+    } else if (message === "-parkour") {
+        system.executeCommand(`tag "${player.name}" add parkour`, () => {});
+        eventData.canceled = true;
+    } else if (message === "-misc") {
+        system.executeCommand(`tag "${player.name}" add misc`, () => {});
+        eventData.canceled = true;
+    } else if (message === "-donoinfo") {
+        system.executeCommand(`tag "${player.name}" add dono`, () => {});
+        eventData.canceled = true;
+    } else if (message === "-enchants") {
+      system.executeCommand(`tag "${player.name}" add ench`, () => {});
+      eventData.canceled = true;
+    } else if (message === "-help") {
+      system.executeCommand(`tag "${player.name}" add help`, () => {});
+      eventData.canceled = true;
+  }
+});
